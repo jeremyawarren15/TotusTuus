aaecb62bfad563ac0ff981657cbcc8f070d40c6d
Jeremy Warren <jeremyawarren15@gmail.com>
2019-02-06 20:10:03 -0500

    Cleanup seed method

--- TotusTuus.Data/Migrations/Configuration.cs
+++ TotusTuus.Data/Migrations/Configuration.cs
@@ -4,11 +4,10 @@ using Microsoft.AspNet.Identity.EntityFramework;
 namespace TotusTuus.Data.Migrations
 {
     using System;
-    using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<TotusTuus.Data.ApplicationDbContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<ApplicationDbContext>
     {
         public Configuration()
         {
