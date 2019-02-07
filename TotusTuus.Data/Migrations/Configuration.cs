e93d9864e6b4768d44fd483213434f925e29e237
Jeremy Warren <jeremyawarren15@gmail.com>
2019-02-06 19:47:57 -0500

    #30 updated seed method so parishes and users are only created if they don't already exist

--- TotusTuus.Data/Migrations/Configuration.cs
+++ TotusTuus.Data/Migrations/Configuration.cs
@@ -17,37 +17,11 @@ namespace TotusTuus.Data.Migrations
 
         protected override void Seed(TotusTuus.Data.ApplicationDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
-
             CreateRoles(context);
 
             CreateUsers(context);
 
-            var parish = new Parish()
-            {
-                ParishName = "St. John the Evangelist",
-                City = "Indianapolis",
-                Archdiocese = "Indianapolis",
-                CreatedDate = DateTimeOffset.UtcNow,
-                OfficePhoneNumber = "317-635-2021",
-                Pastor = "Fr. Rick Nagel",
-                PostalCode = "46225",
-                State = "IN",
-                StreetAddress = "126 W. Georgia St."
-            };
-
-            context.Parishes.Add(parish);
+            CreateParish(context);
         }
 
         private void CreateRoles(ApplicationDbContext context)
@@ -73,17 +47,44 @@ namespace TotusTuus.Data.Migrations
 
         private static void CreateUsers(ApplicationDbContext context)
         {
+            
             var store = new UserStore<ApplicationUser>(context);
             var manager = new UserManager<ApplicationUser>(store);
 
             var superAdmin = new ApplicationUser { FirstName = "Super", LastName = "User", FullName = "Super User", UserName = "super@test.com", Email = "super@test.com" };
             var admin = new ApplicationUser { FirstName = "Admin", LastName = "User", FullName = "Admin User", UserName = "admin@test.com", Email = "admin@test.com" };
 
-            manager.Create(superAdmin, "Test1234!");
-            manager.AddToRole(superAdmin.Id, "SuperAdmin");
+            if (!context.Users.Any(u => u.Email == superAdmin.Email))
+            {
+                manager.Create(superAdmin, "Test1!");
+                manager.AddToRole(superAdmin.Id, "SuperAdmin");
+            }
 
-            manager.Create(admin, "Test1234!");
-            manager.AddToRole(admin.Id, "Admin");
+            if (!context.Users.Any(u => u.Email == admin.Email))
+            {
+                manager.Create(admin, "Test1!");
+                manager.AddToRole(admin.Id, "Admin");
+            }
+        }
+
+        private static void CreateParish(ApplicationDbContext context)
+        {
+            if (context.Parishes.Any()) return;
+
+            var parish = new Parish()
+            {
+                ParishName = "St. John the Evangelist",
+                City = "Indianapolis",
+                Archdiocese = "Indianapolis",
+                CreatedDate = DateTimeOffset.UtcNow,
+                OfficePhoneNumber = "317-635-2021",
+                Pastor = "Fr. Rick Nagel",
+                PostalCode = "46225",
+                State = "IN",
+                StreetAddress = "126 W. Georgia St."
+            };
+
+            context.Parishes.AddOrUpdate(parish);
         }
     }
 }
